c31c8ce8e199b0fd06bf4e4987ea1ee0bc8c1594
cally.cagney@gmail.com <cally.cagney@gmail.com>
2016-01-28 09:18:06 +0000

    Stuff for the game

---
+++ End Game and Leaderboard/Definitions.h
@@ -0,0 +1,32 @@
+#ifndef __TRANSITION_TIME_H__
+#define __TRANSITION_TIME_H__
+
+
+#define TRANSITION_TIME 0.5
+
+#endif // __TRANSITION_TIME_H__
+
+
+/*
+Symbols if needed
+-
+*
+56
+!
+“
+£
+%
+<>
+/
+\
+‘
+@
+#
+~
+=
++
+_
+?
+|
+
+*/
